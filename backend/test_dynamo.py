3968c4b9322bec82eb3a8872cd3fa6cc0ecf6200
Shravan-Parigi <shravan.parigi@gmail.com>
2024-07-12 15:34:33 -0700

    Added functionality to dynamodb

--- backend/test_dynamo.py
+++ backend/test_dynamo.py
@@ -1,6 +1,15 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import datetime
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+
 
 # Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
@@ -15,24 +24,39 @@ table = dynamodb.Table(table_name)
 item = {
     "category": "Food",
     "user_id": "1",
-    "description": "Chipotle",
-    "price": "12",
+    "description": "Voyager Coffee",
+    "price": "5.50",
     "timestamp": str(datetime.datetime.now())
 }
 
-# # Write the item to the table
-# response = table.put_item(Item=item)
+@app.route('/upload', methods=['GET'])
+def upload_chipotle():
+    # Write the item to the table
+    response = table.put_item(Item=item)
 
-# # Check the response
-# if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-#     print("Item successfully written to DynamoDB")
-# else:
-#     print("Error writing item to DynamoDB")
+    # Check the response
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        print("Item successfully written to DynamoDB")
+    else:
+        print("Error writing item to DynamoDB")
+    
+    return "Item successfully written to DB"
 
-r = table.scan(
-    FilterExpression=Attr('category').eq("Food")
-)
-items = r['Items']
-print(items)
-print(len(items))
+@app.route('/getItems', methods=['GET'])
+def get_items():
+    r = table.scan(
+        FilterExpression=Attr('category').eq("Food")
+    )
+    items = r['Items']
+    return str(items)
+
+# r = table.scan(
+#     FilterExpression=Attr('category').eq("Food")
+# )
+# items = r['Items']
+# print(items)
+# print(len(items))
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
