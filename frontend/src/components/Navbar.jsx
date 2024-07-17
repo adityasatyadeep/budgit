2482ec84a16ae73487579f1bf451453dd7fca601
Aditya Satyadeep <adityasatyadeep2002@gmail.com>
2024-07-17 16:55:35 -0700

    testing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
 
     const linkClass = ({isActive}) => isActive 
     ? 'text-black bg-pink-600 hover:bg-pink-900 hover:text-white rounded-md px-3 py-2' 
-    : 'text-white bg-black hover:bg-green-900 hover:text-white rounded-md px-3 py-2'
+    : 'text-white bg-black hover:bg-pink-900 hover:text-white rounded-md px-3 py-2'
   
     return (
-        <nav className="bg-black border-b border-green-700">
+        <nav className="bg-black border-b border-pink-700">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex h-20 items-center justify-between">
               <div
-                className="flex flex-1 items-ceßnter justify-center md:items-stretch md:justify-start"
+                className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
               >
                 {/* <!-- Logo --> */}
                 <NavLink>
