6fe5b429c4398fe0d6dd06c023deab90775411c7
Aditya Satyadeep <adityasatyadeep2002@gmail.com>
2024-07-17 19:43:26 -0700

    some changes to form

--- frontend/src/components/FormRow.jsx
+++ frontend/src/components/FormRow.jsx
@@ -32,15 +32,14 @@ import React, { useState, useEffect } from 'react';
 //     )
 // }
 
-
 const FormRow = ({ label, type, name, value, onChange }) => {
     return (
-      <div className="mb-4">
-        <label className="block text-pink-700 text-sm font-bold mb-2" htmlFor={name}>
+      <div className="mb-4 flex items-center">
+        <label className="text-pink-700 text-sm font-bold w-1/3 pr-4 text-right" htmlFor={name}>
           {label}
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-1/2 py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id={name}
           type={type}
           name={name}
