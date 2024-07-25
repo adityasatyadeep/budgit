0e613fc1ee6591fe3e388f2ebad10ab11b40e77c
Shravan-Parigi <shravan.parigi@gmail.com>
2024-07-25 13:06:16 -0700

    tried timeline

--- frontend/src/components/Calendar/DayModal.jsx
+++ frontend/src/components/Calendar/DayModal.jsx
@@ -16,6 +16,14 @@ import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
 const DayModal = ({ open, handleClose, allOn, isSelected, onClick, items }) => {
     const [fullWidth, setFullWidth] = React.useState(true);
     const [maxWidth, setMaxWidth] = React.useState('sm');
@@ -69,9 +77,48 @@ const DayModal = ({ open, handleClose, allOn, isSelected, onClick, items }) => {
             >
                 <DialogTitle>$$$</DialogTitle>
                 <DialogContent>
-                    <DialogContentText>
-                        WOW
-                    </DialogContentText>
+                    <Timeline position="alternate">
+                        <TimelineItem>
+                            <TimelineOppositeContent color="text.secondary">
+                                09:30 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>Eat</TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent color="text.secondary">
+                                10:00 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>Code</TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent color="text.secondary">
+                                12:00 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>Sleep</TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent color="text.secondary">
+                                9:00 am
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>Repeat</TimelineContent>
+                        </TimelineItem>
+                    </Timeline>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} sx={{
