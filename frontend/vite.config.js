a15f69177805d030f872e1528f279aab334dbfd9
Shravan-Parigi <shravan.parigi@gmail.com>
2024-08-08 11:01:47 -0700

    added host functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This binds the server to all network interfaces, including your public IP
+  },
 })
