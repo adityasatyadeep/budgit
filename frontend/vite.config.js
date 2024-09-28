a32261c939681897eaffc147ffaea8f26aaf2d34
Shravan Parigi <shrav1@MacBook-Air.local>
2024-09-27 17:18:58 -0700

    Added network capabilities

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from external IP
+    port: 5173, // Optional: Specify the port you want to use
+  },
 })
